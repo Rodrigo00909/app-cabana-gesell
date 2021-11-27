86428f418733474467a7a8e39628971b72b61791
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-11-27 08:24:56 -0300

    subida banner con slide manual

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import BannerCabañaGesell from '../../assets/Banner-Cabana-Gesell.jpg';
 import Banner2 from '../../assets/naomi-hebert-MP0bgaS_d1c-unsplash.jpg';
@@ -11,12 +11,71 @@ import { ReactComponent as FlechaDer } from '../../assets/iconmonstr-angel-right
 import './HomeBanner.css';
 
 const HomeBanner = () => {
+
+    /* Hook useRef */
+    // Guarda el valor en una referencia (y no estado) para no perder la posición de la foto x mas q se renderize la app
+    const slideshow = useRef(null);
+    /* // Hook useRef */
+
+    const RightSlide = () => {
+        /* console.log(slideshow.current); */ // Current (examinar propiedad consolelog)
+        //Comprobar q slideshow tenga elementos
+        if (slideshow.current.children.length > 0) {
+
+            //primer hijo
+            const firstElement = slideshow.current.children[0];
+
+            slideshow.current.style.transition = `1000ms ease-out all`;
+
+            const tamañoSlide = slideshow.current.children[0].offsetWidth;
+            slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`; // se desplaza 1000px
+
+            const transicion = () => {
+                slideshow.current.style.transition = 'none';
+                slideshow.current.style.transform = `translateX(0)`; // Regresa slide a la pos 0
+
+                slideshow.current.appendChild(firstElement); // manda la foto 1 al final
+
+                slideshow.current.removeEventListener('transitionend', transicion);
+            }
+
+            // ejecuta cuando termina la animacion
+            slideshow.current.addEventListener('transitionend', transicion);
+
+        }
+    }
+
+    const LeftSlide = () => {
+        console.log("Anterior");
+        if (slideshow.current.children.length > 0) {
+
+            const index = slideshow.current.children.length - 1;
+            const lastElement = slideshow.current.children[index];
+
+            slideshow.current.insertBefore(lastElement, slideshow.current.firstChild);
+
+            slideshow.current.style.transition = `none`;
+
+            const tamañoSlide = slideshow.current.children[0].offsetWidth;
+            slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+            setTimeout(() => {
+                slideshow.current.style.transition = `1000ms ease-out all`;
+                slideshow.current.style.transform = `translateX(0)`;
+            }, 30);
+
+        }
+    }
+
     return (
         <div className='home-banner'>
 
-            <section className="home-banner-slideshow">
+            <section className="home-banner-slideshow" ref={slideshow}>
                 <div className='home-banner-slide'>
                     <img src={BannerCabañaGesell} alt="" />
+                    {/* <div className="home-text-slide">
+                        <p>Texto de Prueba</p>
+                    </div> */}
                 </div>
                 <div className='home-banner-slide'>
                     <img src={Banner2} alt="" />
@@ -29,14 +88,15 @@ const HomeBanner = () => {
                 </div>
             </section>
 
-            <div>
-                <button>
+            <div className='home-banner-controls'>
+                <button onClick={LeftSlide} className='home-banner-buttons home-button-iz'>
                     <FlechaIz />
                 </button>
-                <button>
+                <button onClick={RightSlide} className='home-banner-buttons home-button-der'>
                     <FlechaDer />
                 </button>
             </div>
+
         </div>
     )
 }
